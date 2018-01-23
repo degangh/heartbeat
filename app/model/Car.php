ffb00bec2135e593e222dad9dca294a9169c22ea
degangh <degang.huang@gmail.com>
2018-01-24 10:11:11 +1030

    code change for testing only, remove later

---
+++ app/model/Car.php
@@ -0,0 +1,6 @@
+<?php
+namespace \Car;
+
+class Car extends Model{
+    
+}
--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-spl_autoload_register(function ($className)
-{
-    $path = 'class/' . $className . ".class.php";
-
-    include $path;
-});
 
 
 
