45932cbf487fc8d279f654a9d94d8a5cc68f745d
degangh <degang.huang@gmail.com>
2018-05-31 17:01:42 +0930

    check if route is defined

--- core/Router.php
+++ core/Router.php
@@ -11,14 +11,29 @@ class Router
 
     public function go($uri)
     {
-        $handler = $this->routes[$uri];
+        try
+        {
+            if (!array_key_exists($uri,$this->routes)) throw new \Exception("No route defined");
+            $handler = $this->routes[$uri];
+        
+            $controller = explode("@", $handler)[0];
+            $method = explode("@", $handler)[1];
 
-        $controller = explode("@", $handler)[0];
-        $method = explode("@", $handler)[1];
+            $controller = "app\controller\\".$controller;
+            
+            if (!class_exists($controller)) throw new \Exception("Module not defined");
 
-        $controller = "app\controller\\".$controller;
-        $c = new $controller();
-        $c->$method();
+            $c = new $controller();
+
+            if (!method_exists($c, $method)) throw new \Exception("Action not defined");
+
+            $c->$method();
+        }
+        catch(\Exception $e)
+        {
+            var_dump($e->getMessage());
+        }
+        
     }
 
 }
